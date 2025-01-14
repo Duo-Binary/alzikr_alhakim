409d679226a79ebaf541199a6043cdf85c627f72
mohamdelbehairy <rahmawemohamed@gmail.com>
2025-01-14 22:13:50 +0200

    add styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Styles {
+  static TextStyle bold30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: Colors.black,
+  );
+
+  static TextStyle regular15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.normal,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: Colors.white,
+  );
+
+  static TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: Colors.white,
+  );
+
+  static TextStyle extraLight12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: Colors.black,
+  );
+
+  static TextStyle semiBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: Colors.black,
+  );
+
+  static TextStyle medium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: const Color(0xff240F4F),
+  );
+
+  static TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    color: const Color(0xffB121BF),
+  );
+}
